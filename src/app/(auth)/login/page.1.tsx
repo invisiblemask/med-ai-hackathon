cf1a7b612596ea0ee38b004e14911a7cb65d9e39
invisiblemask <okekekelechi96@gmail.com>
2024-03-24 17:18:19 +0100

    worked on all authentication pages

--- src/app/(auth)/login/page.1.tsx
+++ src/app/(auth)/login/page.1.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { Button, Input } from "@nextui-org/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -7,7 +6,7 @@ import React, { FormEvent, useState } from "react";
 
 export default function page() {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   function onSubmit(e: FormEvent) {
     e.preventDefault();
@@ -17,7 +16,7 @@ export default function page() {
     <div className="flex flex-col lg:flex-row m-4">
       <div className="bg-primary rounded-[20px] min-h-screen w-full lg:flex flex-col hidden items-center justify-center flex-1 gap-5">
         <Image
-          src="/images/wallet-page-image.svg"
+          src="/images/login-logo.svg"
           alt="login image"
           width={1200}
           height={980}
@@ -30,7 +29,7 @@ export default function page() {
       <div className="flex flex-col flex-1 p-0 lg:p-10 lg:items-center lg:justify-center min-h-screen relative">
         <div
           className="absolute flex flex-row gap-2 items-center text-[#5D6883] top-4 lg:left-20 left-0 cursor-pointer"
-          onClick={() => router.back()}>
+          onClick={() => router.push("/")}>
           <Image
             src="/icons/left-arrow.svg"
             alt="left arrow icon"
@@ -40,16 +39,13 @@ export default function page() {
           />
           Back
         </div>
-        <div className="flex flex-col gap-10 mt-28">
+        <div className="flex flex-col gap-10 my-auto">
           <div className="flex flex-col gap-2">
-            <h1 className="text-3xl font-normal flex flex-col gap-1 text-start">
-              Forgot password
+            <h1 className="text-3xl font-normal flex flex-col gap-1">
+              Welcome to MedAi. <span>Sign in to get started.</span>
             </h1>
-            <p className="text-sm text-[#8181A5] flex flex-col gap-1">
-              Forgot your password ? No worries.
-              <span>
-                Just enter your email and we will send you a link to reset it.
-              </span>
+            <p className="text-sm text-[#8181A5]">
+              Enter your details to proceed further
             </p>
           </div>
           <div className="flex flex-col gap-4 lg:w-[414px]">
@@ -59,14 +55,28 @@ export default function page() {
                 placeholder="johndoe@example.com"
                 type="text"
                 labelPlacement="outside"
-                classNames={{ inputWrapper: "h-[50px]", label: "text-base" }}
+                classNames={{
+                  innerWrapper: "h-[50px]"
+                }}
               />
+
+              <div className="flex flex-col gap-2">
+                <Input
+                  label="Password :"
+                  placeholder="********"
+                  type="password"
+                  labelPlacement="outside"
+                  classNames={{
+                    input: "h-[50px]"
+                  }}
+                />
+              </div>
 
               <Button
                 isLoading={isLoading}
                 color="primary"
                 spinnerPlacement="end">
-                {isLoading ? "Wait a minute" : "Submit"}
+                {isLoading ? "Wait a minute" : "Sign in"}
               </Button>
             </form>
           </div>
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,42 +1,5 @@
-import { Button } from "@nextui-org/react";
-import Image from "next/image";
-import React from "react";
+import { LoginComponent } from "../component";
 
 export default function page() {
-  return (
-    <div className="flex flex-col lg:flex-row m-4">
-      <div className="bg-primary rounded-[20px] min-h-screen w-full lg:flex flex-col hidden items-center justify-center flex-1 gap-5">
-        <Image
-          src="/images/login-logo.svg"
-          alt="login image"
-          width={1200}
-          height={980}
-          className="w-auto h-auto"
-        />
-        <h1 className="text-4xl text-center text-white w-3/5 leading-relaxed">
-          Get Easy access to patient images and reports
-        </h1>
-      </div>
-      <div className="flex flex-col flex-1 p-4 lg:p-10 items-center justify-center">
-        <div className="flex flex-col gap-10">
-          <div className="flex flex-col gap-2">
-            <h1 className="text-3xl font-normal flex flex-col gap-1">
-              Welcome to MedAi. <span>Sign in to get started.</span>
-            </h1>
-            <p className="text-sm text-[#8181A5]">
-              Enter your details to proceed further
-            </p>
-          </div>
-          <div className="flex flex-col gap-4 w-[414px]">
-            <Button color="primary" size="lg" radius="lg">
-              Connect your wallet
-            </Button>
-            <Button color="primary" size="lg" radius="lg">
-              Google sign in
-            </Button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <LoginComponent />;
 }

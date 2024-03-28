9f4988411f34f132d8ec32caf852b80484c5c7c1
Chioma Ezeamama <58475309+Chiomsyn@users.noreply.github.com>
2024-03-28 21:29:11 +0100

    fix

--- src/app/(auth)/connect-wallet/page.tsx
+++ src/app/(auth)/connect-wallet/page.tsx
@@ -1,13 +1,29 @@
 "use client";
 
+import { useSDK } from "@metamask/sdk-react";
 import { Button } from "@nextui-org/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
+
+
 
 export default function page() {
   const router = useRouter();
+  const [account, setAccount] = useState<any>();
+    const { sdk, connected, connecting, provider, chainId } = useSDK();
+
+    const connect = async () => {
+        try {
+            const accounts: any = await sdk?.connect();
+            setAccount(accounts?.[0]);
+        } catch (err) {
+            console.warn("failed to connect..", err);
+        }
+    };
+
+    console.log(account)
   return (
     <div className="flex flex-col lg:flex-row m-4">
       <div className="bg-primary rounded-[20px] min-h-screen w-full lg:flex flex-col hidden items-center justify-center flex-1 gap-5">
@@ -45,7 +61,7 @@ export default function page() {
             </p>
           </div>
           <div className="flex flex-col gap-8 lg:w-96">
-            <div className="bg-[#F4F6F9] h-[76px] flex flex-row items-center justify-between px-8 rounded-lg cursor-pointer">
+            <div className="bg-[#F4F6F9] h-[76px] flex flex-row items-center justify-between px-8 rounded-lg cursor-pointer" onClick={connect}>
               <div className="flex flex-row gap-4 items-center bg-gradient-to-br from-teal-500 to-teal-400 bg-clip-text text-transparent">
                 <Image
                   src="/icons/MetaMask_Fox 1.svg"
--- src/app/components/auth-button.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-
-const AuthButton = () => {
-    return <ConnectButton />;
-
-}
-
-export default AuthButton
